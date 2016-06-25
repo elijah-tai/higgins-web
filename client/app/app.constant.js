406d18679ef2f445262eb038e220f7876a24412c
WonJune Tai <wonjune.tai@gmail.com>
2016-06-25 19:20:17 -0400

    Improvements to datetime picker for reminders
    * CSS changes to body element to accommodate the fixed navbar
    * add switch for reminder recurrences
    
    Resolves issue #59

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -1,6 +1,7 @@
 (function(angular, undefined) {
   angular.module("higginsApp.constants", [])
 
+.constant("moment", moment)
 .constant("appConfig", {
 	"userRoles": [
 		"guest",
--- client/components/modals/reminderModal/reminderModal.controller.js
+++ client/components/modals/reminderModal/reminderModal.controller.js
@@ -17,6 +17,32 @@ class ReminderModalController {
       doesRecur: false,
       recurType: 'Select One'
     };
+
+    var self = this;
+
+    // date picker
+    this.dateOptions = {
+      min: new Date(),
+      format: 'mmmm d, yyyy',
+      onSet: function(e) {
+        // e.select is number of milliseconds since Unix Epoch
+        self.date = new Date(e.select);
+      }
+    };
+
+    // time picker
+    this.timeOptions = {
+      interval: 1,
+      closeOnSelect: true,
+      onSet: function(e) {
+        if (!!self.date) {
+          // e.select is number of minutes after midnight
+          var date = self.date;
+          var secondsSinceEpoch = date.setMinutes(e.select);
+          self.reminder.datetime = new Date(secondsSinceEpoch);
+        }
+      }
+    };
   }
 
   openReminderDateTimePicker(e) {
